0cda8429fc92d5640fc81eb92f47fa009e25f3af
kreibinger <riabinin.kf.career@icloud.com>
2025-08-27 19:27:53 +0200

    replaced RegisteredClaims with MapClaims to support refresh tokens

--- internal/auth/authenticator.go
+++ internal/auth/authenticator.go
@@ -35,6 +35,7 @@ func NewAuthenticator(
 }
 
 type (
+	// Authenticator is responsible for JWT authentication and validation of requests.
 	Authenticator struct {
 		secret string
 		log    *logger.Logger
@@ -48,6 +49,8 @@ type (
 	}
 )
 
+// Authenticate validates the Authorization header from the request
+// and returns the token's subject (user_id) or an error if invalid.
 func (a *Authenticator) Authenticate(_ context.Context, req *http.Request) (string, error) {
 	var (
 		err   error
@@ -60,6 +63,8 @@ func (a *Authenticator) Authenticate(_ context.Context, req *http.Request) (stri
 	return token, nil
 }
 
+// parseToken extracts and validates the subject of a JWT token from the Authorization header.
+// Returns the subject or an error.
 func (a *Authenticator) parseToken(authHeader string) (string, error) {
 	if !strings.HasPrefix(authHeader, "Bearer ") {
 		return "", ErrTokenMissing
@@ -69,7 +74,7 @@ func (a *Authenticator) parseToken(authHeader string) (string, error) {
 
 	var (
 		err         error
-		claims      jwt.RegisteredClaims
+		claims      jwt.MapClaims
 		parsedToken *jwt.Token
 	)
 	if parsedToken, err = a.parser.ParseWithClaims(token, &claims, func(token *jwt.Token) (any, error) {
@@ -86,13 +91,23 @@ func (a *Authenticator) parseToken(authHeader string) (string, error) {
 		return "", errors.New("invalid token")
 	}
 
-	if claims.Subject == "" {
+	if tokenType, ok := claims["typ"].(string); !ok || tokenType != tokenNameAccessToken {
+		return "", errors.New("invalid token type")
+	}
+
+	var (
+		ok      bool
+		subject string
+	)
+	if subject, ok = claims["sub"].(string); !ok || subject == "" {
 		return "", errors.New("subject is missing")
 	}
 
-	return claims.Subject, nil
+	return subject, nil
 }
 
+// isAuthRequired determines if authentication is required for the given HTTP request
+// based on the noAuthRoutes config.
 func (a *Authenticator) isAuthRequired(req *http.Request) bool {
 	_, ok := a.noAuthRoutes[fmt.Sprintf("%s %s", req.Method, req.URL.Path)]
 	return !ok
